8d61eb14b0d8d044c27cab8fabb9f6b256764dd4
Abhishek <abhishek@imoneypay.in>
2024-03-21 18:30:00 +0530

    other changes

--- Mongo/MongoConnection.js
+++ Mongo/MongoConnection.js
@@ -1,14 +1,44 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-module.exports = async () => {
+// module.exports = async () => {
+//   try {
+//     await mongoose.set("strictQuery", false);
+//     await mongoose.connect(
+//       "mongodb+srv://singh80020:56Q3oYCvbkQTBn2Y@my-lappy.p4sxzae.mongodb.net/",
+//       { replicaSet: "rs0", readPreference: "primary", w: "majority" }
+//     );
+//     console.log("connected to db");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit();
+//   }
+// };
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://singh80020:56Q3oYCvbkQTBn2Y@my-lappy.p4sxzae.mongodb.net/?retryWrites=true&w=majority&appName=my-lappy";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
   try {
-    await mongoose.set("strictQuery", false);
-    await mongoose.connect(
-      "mongodb+srv://singh80020:56Q3oYCvbkQTBn2Y@my-lappy.p4sxzae.mongodb.net/"
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
     );
-    console.log("connected to db");
-  } catch (error) {
-    console.log(error);
-    process.exit();
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 };
+module.exports = { run };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { run } = require("./Mongo/MongoConnection");
 const app = require("./src/Services");
-const dbConnection = require("./Mongo/MongoConnection");
+// const dbConnection = require("./Mongo/MongoConnection");
 const port = 3001;
 
 //password 56Q3oYCvbkQTBn2Y
@@ -7,6 +8,7 @@ const port = 3001;
 // link mongodb+srv://singh80020:56Q3oYCvbkQTBn2Y@my-lappy.p4sxzae.mongodb.net/
 
 app.listen(port, async () => {
-  await dbConnection();
+  // await dbConnection();
+  await run();
   console.log(` app listening on port ${port}`);
 });
