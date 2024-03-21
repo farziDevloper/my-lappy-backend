f0d2be082f5f65cb21a947b342cf5f440bf76913
Abhishek <abhishek@imoneypay.in>
2024-03-21 23:19:05 +0530

    other changes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { run } = require("./Mongo/MongoConnection");
 const app = require("./src/Services");
-// const dbConnection = require("./Mongo/MongoConnection");
+const dbConnection = require("./Mongo/MongoConnection");
 const port = 3001;
 
 //password 56Q3oYCvbkQTBn2Y
 // username singh80020
 // link mongodb+srv://singh80020:56Q3oYCvbkQTBn2Y@my-lappy.p4sxzae.mongodb.net/
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Mongo");
+});
+
 app.listen(port, async () => {
-  // await dbConnection();
-  await run();
+  await dbConnection();
   console.log(` app listening on port ${port}`);
 });
