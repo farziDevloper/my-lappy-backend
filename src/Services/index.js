b4e2eb368e3ff1dab8d98c80d75eff7b6a4bf2a2
Abhishek <abhishek@imoneypay.in>
2024-03-22 10:26:17 +0530

    new commit

--- src/Services/index.js
+++ src/Services/index.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 const userController = require("../Controllers/user.controller");
+const tokenController = require("../Controllers/token.controller");
 
 app.use("/user", userController);
+app.use("/token", tokenController);
+console.log("Welcome to the API!", 1);
 
 module.exports = app;
